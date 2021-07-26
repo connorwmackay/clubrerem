96ba0692bb0902f2e39dbdb74d1e70fdd6731df1
Connor Mackay <44244596+connorwmackay@users.noreply.github.com>
2021-07-26 15:37:58 +0100

    Added docker-compose and setup basic layout
    Added docker-compose as well as setup a basic layout for pages in pug.

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ WORKDIR /app
 COPY ["package.json", "package-lock.json*", "./"]
 RUN npm install
 COPY . .
-CMD [ "node", "app.js" ]
+CMD [ "npm", "start" ]
--- app.js
+++ app.js
@@ -7,7 +7,11 @@ app.use(express.static('public'))
 
 // TODO: Give each route their own JS file instead of being put in app.js
 app.get('/', (req, res) => {
-    res.render('index', {page_title: 'Club ReRem', page_name: 'Club ReRem'});
+    res.render('index', {page_title: 'Club ReRem', page_head: 'Club ReRem'});
+});
+
+app.get('/bar', (req, res) => {
+    res.render('bar', {page_title: 'Club ReRem - Virtual Bar', page_head: 'Virtual Bar'});
 });
 
 app.listen(3000, () => {
